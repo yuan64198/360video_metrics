3feb674bbb7471776b0c32e80026c84537d8c6a7
yuan64198 <yuan64198@gmail.com>
2018-05-15 00:42:40 +0800

    update WSPSNR

--- WSPsnrCalc.py
+++ WSPsnrCalc.py
@@ -3,21 +3,26 @@ import math
 from PIL import Image
 
 PIXEL_MAX = 255.0
-
+#Input should be ndarrays
 def WSPsnrCalc(img1, img2):
-    width, height = img1.size
-    img1_list = list(img1.getdata())
-    img2_list = list(img2.getdata())
-    img1_list = [img1_list[i * width:(i + 1) * width] for i in range(height)]
-    img2_list = [img2_list[i * width:(i + 1) * width] for i in range(height)]
-    weight_list = [[math.cos((j+0.5-height/2)*math.pi/height) for j in range(height)] for i in range(width)]
-    weight_sum = sum(sum(weight_list,[]))
-    wmse = 0
+    height, width, channel = img1.shape
+    print("aaa")
+    weight_list = [[math.cos((i+0.5-height/2)*math.pi/height) for j in range(width)] for i in range(height)]
+    print("ccc")
+    weight_sum = 0.0
+    wmse = 0.0
     for i in range(height):
+	print(i)
         for j in range(width):
-            for k in range(3):#RGB
-                wmse += ((img1_list[i][j][k] - img2_list[i][j][k])**2)*weight_list[i][j]
-    wmse = (wmse/(width*height))/3
+            for k in range(channel):#RGB
+		if(weight_list[i][j] < 0):
+		    print("fuck!!!")
+		    return 99999
+	        wmse += ((img1[i][j][k] - img2[i][j][k])**2)*weight_list[i][j]/channel
+	    weight_sum += weight_list[i][j]
+    wmse = (wmse/(width*height))/channel
+    print("MSE:")
+    print(wmse)
     if wmse == 0:
         return 100
     ws_psnr = 10*numpy.log10((PIXEL_MAX**2 /wmse))
